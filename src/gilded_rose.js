f4eed69e90b024d01deb4d42abf458470120f4c4
nimzyow <n_soufiani@hotmail.com>
2020-04-22 22:18:37 +0100

    dried out strings

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -11,19 +11,18 @@ class Shop {
     this.items = items;
   }
   updateQuality() {
+    const AGED_BRIE = "Aged Brie";
+    const BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert";
     for (let i = 0; i < this.items.length; i++) {
       if (
-        this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+        this.items[i].name != AGED_BRIE &&
+        this.items[i].name != BACKSTAGE_PASS
       ) {
-        //below two inner functions are checking for quality
         this.checkForQuality(this.items[i]);
       } else {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
+          if (this.items[i].name == BACKSTAGE_PASS) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
                 this.items[i].quality = this.items[i].quality + 1;
@@ -41,10 +40,8 @@ class Shop {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
+        if (this.items[i].name != AGED_BRIE) {
+          if (this.items[i].name != BACKSTAGE_PASS) {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
                 this.items[i].quality = this.items[i].quality - 1;
